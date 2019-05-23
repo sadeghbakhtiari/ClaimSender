071e384c8ed143701d424a762562f259c5c78787
emaddarvishi <emad.darvishi@yahoo.com>
2019-05-23 23:37:52 +0430

    servcie

--- src/main/java/com/asgarie/ClaimSender/service/api/AskarService.java
+++ src/main/java/com/asgarie/ClaimSender/service/api/AskarService.java
@@ -11,4 +11,33 @@ public interface AskarService {
 
     List<PatientBillMessageVO> buildMessageVo();
 
+    public PatientBillMessageVO convertAdmissionVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertBasicDeathDetailsVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertBillSummaryVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertCauseVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertDiagnosisVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertDischargeVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertHealthcareProviderVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertHighLevelAreaVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertHospitalWardVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertInsuranceVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertOrganizationVo(Integer sepasId);
+
+    public PatientBillMessageVO convertPersonInfoVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertServiceDetailesVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convertServiceGroupRowVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
+
+    public PatientBillMessageVO convert(PatientBillMessageVO patientBillMessageVO, Integer sepasId);
 }
--- src/main/java/com/asgarie/ClaimSender/service/impl/AskarServiceImpl.java
+++ src/main/java/com/asgarie/ClaimSender/service/impl/AskarServiceImpl.java
@@ -49,31 +49,107 @@ public class AskarServiceImpl implements AskarService {
         List<PatientBillMessageVO> messageVOList = new ArrayList<>();
         transferList.forEach(t -> {
             PatientBillMessageVO messageVO = new PatientBillMessageVO();
-            PersonInfoVo person = personDao.getById(t.getSepasId());
-            PersonInfoVO personInfoVO = new PersonInfoVO();
-            DODATE bDate = new DODATE();
-            bDate.setDay(person.getBirthDay());
-            bDate.setMonth(person.getBirthMonth());
-            bDate.setYear(person.getBirthYear());
-            personInfoVO.setBirthDate(bDate);
-            personInfoVO.setFirstName(person.getFirstName());
-            personInfoVO.setLastName(person.getLastName());
-            personInfoVO.setNationalCode(person.getNationalCode());
-            messageVO.setPerson(personInfoVO);
             BillPatientCompositionVO compositionVO = new BillPatientCompositionVO();
-            AdmissionVO admissionVO = new AdmissionVO();
-            AdmissionVo admission = admissionDao.getById(t.getSepasId());
-            admissionVO.setMedicalRecordNumber(admission.getMedicalRecordNumber());
             OrganizationVO organizationVO = new OrganizationVO();
             OrganizationVo org = organizationDao.getById(1);
             organizationVO.setName(org.getName());
-            admissionVO.setInstitute(organizationVO);
-            compositionVO.setAdmission(admissionVO);
             messageVO.setComposition(compositionVO);
 
             messageVOList.add(messageVO);
         });
 
         return messageVOList;
+    }
+
+    @Override
+    public PatientBillMessageVO convertAdmissionVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        AdmissionVO admissionVO = new AdmissionVO();
+        AdmissionVo admission = admissionDao.getById(sepasId);
+        admissionVO.setMedicalRecordNumber(admission.getMedicalRecordNumber());
+        admissionVO.setInstitute(convertOrganizationVo(sepasId));
+
+        patientBillMessageVO.getComposition().setAdmission(admissionVO);
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertBasicDeathDetailsVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertBillSummaryVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertCauseVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertDiagnosisVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertDischargeVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertHealthcareProviderVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertHighLevelAreaVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertHospitalWardVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertInsuranceVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertOrganizationVo(Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertPersonInfoVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        PersonInfoVo person = personDao.getById(t.getSepasId());
+        PersonInfoVO personInfoVO = new PersonInfoVO();
+        DODATE bDate = new DODATE();
+        bDate.setDay(person.getBirthDay());
+        bDate.setMonth(person.getBirthMonth());
+        bDate.setYear(person.getBirthYear());
+        personInfoVO.setBirthDate(bDate);
+        personInfoVO.setFirstName(person.getFirstName());
+        personInfoVO.setLastName(person.getLastName());
+        personInfoVO.setNationalCode(person.getNationalCode());
+        patientBillMessageVO.setPerson(personInfoVO);
+        return patientBillMessageVO;
+    }
+
+    @Override
+    public PatientBillMessageVO convertServiceDetailesVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convertServiceGroupRowVo(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
+    }
+
+    @Override
+    public PatientBillMessageVO convert(PatientBillMessageVO patientBillMessageVO, Integer sepasId) {
+        return null;
     }
 }
